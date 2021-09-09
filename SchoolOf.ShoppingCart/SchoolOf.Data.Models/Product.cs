a70c9b98ef599b2b19dd6271277b3ec44705c7e2
Dumitrascu Sandra-Maria-Monica <dumitrascusandra18@stud.ase.ro>
2021-09-09 10:27:13 +0300

    adding project

---
+++ SchoolOf.ShoppingCart/SchoolOf.Data.Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using SchoolOf.Data.Abstraction;
+using System;
+
+namespace SchoolOf.Data.Models
+{
+    public class Product : BaseEntityModel
+    {
+       
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+        public string Image { get; set; }
+        public long Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsDeleted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTimeOffset CreatedAt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTimeOffset ModifiedAt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        //toate modelele vor avea met de mai jos +long id
+
+    }
+}
---
+++ SchoolOf.ShoppingCart/SchoolOf.ShoppingCart/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolOf.ShoppingCart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //este un controller API nu avem view
+    public class ProductsController : ControllerBase
+    {
+        //returneaza o lista de produse statice
+        [HttpGet] //verbe pt a initia un request http
+        [ProducesResponseType(typeof(IEnumerable<ShoolOf.Dtos.ProductDto>),200)]
+        public async Task<IActionResult> getProducts()
+        {
+            //iactionresult=returneaza o lista mai ampla de modele
+            //async=thread-ul care a pornit procesarea se intoarce in threadpool, comunicare asincrona
+            var mylistofPtoducts = new List<ShoolOf.Dtos.ProductDto>();
+            mylistofPtoducts.Add(new ShoolOf.Dtos.ProductDto
+            {
+                Category = "test category",
+                Description = " test description",
+                Id = 10,
+                Image="no image yet",
+                Name="test product",
+                Price=100
+            }) ;
+            return Ok(mylistofPtoducts); //(sau not found) ok= returneaza un hhtp status 200
+            
+            
+        }
+             
+    }
+}
